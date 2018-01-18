ec9abcffc9d3ecdef994353e1e92c671739a8740
gurustron <gurustronpublic@gmail.com>
2018-01-19 02:43:46 +0300

    2.4.6. os.walk

--- python/StepikPy512/2/2.4.6.file_dir_structure.py
+++ python/StepikPy512/2/2.4.6.file_dir_structure.py
@@ -1,4 +1,15 @@
 import os
 import os.path
+import re
 
-print(os.getcwd())
+r = "D:/Stron/Projects/"
+fld = "main"
+lines = []
+
+with open("2.4.6.A", "w+") as f:
+    for root, d, files in os.walk(r + fld):
+        if any(f.endswith(".py") for f in files):
+            lines.append(re.sub(r, '', root, flags=re.IGNORECASE))
+            print(root)
+    print(lines)
+    f.write("\n".join(lines))
