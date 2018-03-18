d475152fcd3f66fd73a7ecf3421d352a2b743351
gurustron <gurustronpublic@gmail.com>
2018-03-19 01:27:19 +0300

    xml cubes

---
+++ python/StepikPy512/3/3.7.4.xml_cubes.py
@@ -0,0 +1,20 @@
+from xml.etree import ElementTree
+
+root = ElementTree.fromstring('<cube color="blue"><cube color="red"><cube color="green"></cube></cube><cube color="red"></cube></cube>')
+
+print(root.attrib)
+res = {}
+queue = [(root, 1)]
+
+while len(queue) > 0:
+    curr = queue.pop()
+
+    elem = curr[0]
+    lvl = curr[1]
+    new_lvl = lvl + 1
+    for child in elem:
+        queue.append((child, new_lvl))
+    key = elem.attrib['color']
+    res[key] = res.get(key, 0) + lvl
+
+print(" ".join(str(x) for x in [res['red'], res['green'], res['blue']]))
