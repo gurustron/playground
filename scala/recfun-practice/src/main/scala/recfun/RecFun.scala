c574c0a6bc17f4215816a2c421f625f1ed0bb485
gurustron <gurustronpublic@gmail.com>
2020-02-21 00:29:26 +0300

    week 1 submission

--- scala/recfun-practice/src/main/scala/recfun/RecFun.scala
+++ scala/recfun-practice/src/main/scala/recfun/RecFun.scala
@@ -54,5 +54,15 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0){
+      1
+    }
+    else if(money < 0 || coins.isEmpty) {
+      0
+    }
+    else{
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
