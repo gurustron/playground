b0e7c5f2d6df1998331e057df3200b3c27dd24df
gurustron <gurustronpublic@gmail.com>
2020-04-02 21:59:06 +0300

    poly

--- scala/coursera-course/second/calculator/src/main/scala/calculator/Polynomial.scala
+++ scala/coursera-course/second/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,16 @@ package calculator
 object Polynomial extends PolynomialInterface {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(math.pow(b(), 2) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Var(
+      if(delta() < 0) Set() else
+      Set(
+      (-b() + math.pow(delta(), 0.5)) / (2 * a()),
+      (-b() - math.pow(delta(), 0.5)) / (2 * a())
+    ))
   }
 }
