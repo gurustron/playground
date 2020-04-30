7b71142f1c487ddeda2e849389c2d19233e1a531
gurustron <gurustronpublic@gmail.com>
2020-04-30 20:17:28 +0300

    seed

---
+++ elixir/pluralsight/rsvp_umbrella/apps/rsvp/priv/repo/seed.exs
@@ -0,0 +1,4 @@
+unless(Rsvp.EventsQueries.any) do 
+    Rsvp.EventsQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{eventOn: DateTime.utc_now |> DateTime.add(3600*24*365, :second), title: "Summer Codefest", location: "Omaha, NE"}))
+    Rsvp.EventsQueries.create(Rsvp.Events.changeset(%Rsvp.Events{}, %{eventOn: DateTime.utc_now |> DateTime.add(3600*24* 30, :second), title: "Summer Codefest", location: "London"}))
+end
