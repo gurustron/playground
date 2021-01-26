52e6f4b731d26bc41810fff0c36876c857c0149d
gurustron <gurustronpublic@gmail.com>
2021-01-26 18:11:56 +0300

    fintech test

---
+++ csharp/fintech.exam/FintechExam/FintechExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FintechExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
