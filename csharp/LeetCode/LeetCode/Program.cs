ed938169931b523d74bc1a1e7a5db495022b0ee6
gurustron <gurustronpublic@gmail.com>
2021-09-07 21:56:43 +0300

    IncrementLargeDigitArray https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/

--- csharp/LeetCode/LeetCode/Program.cs
+++ csharp/LeetCode/LeetCode/Program.cs
@@ -3,14 +3,11 @@ using LeetCode;
 
 Console.WriteLine("Hello World!");
 
-var data = new []
-{
-    // (2, new[]{1,2,3}),
-    (2, new []{-1,-100,3,99})
-};
+var solution = new IncrementLargeDigitArray.Solution();
 
-foreach (var (k, ints) in data)
-{
-    new RotateArray().Rotate(ints, k);
-    Console.WriteLine(string.Join(',', ints));
-}
+PrintResult(solution.PlusOne(new []{9}));
+PrintResult(solution.PlusOne(new []{1,2,3,6}));
+PrintResult(solution.PlusOne(new []{0}));
+
+void PrintResult(int[] array) => Console.WriteLine(array.ToPrintVersion());
+
