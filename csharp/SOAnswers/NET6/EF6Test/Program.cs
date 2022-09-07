9f5ed81812b4f0f19aa82593328bf271b6951732
gurustron <gurustronpublic@gmail.com>
2022-09-08 02:12:03 +0300

    --

--- csharp/SOAnswers/NET6/EF6Test/Program.cs
+++ csharp/SOAnswers/NET6/EF6Test/Program.cs
@@ -1,34 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello, World!");
+
 var serviceCollection = new ServiceCollection();
-serviceCollection.AddDbContext<SomeContext>(builder => builder.UseSqlite($"Filename={nameof(SomeContext)}.db"));
+serviceCollection.AddDbContext<SomeContext>(builder => 
+    builder.UseSqlite($"Filename={nameof(SomeContext)}.db", opts => opts.CommandTimeout(1000) )
+        .LogTo(Console.WriteLine), ServiceLifetime.Transient);
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
 using (var scope = serviceProvider.CreateScope())
 {
     var ctx = scope.ServiceProvider.GetService<SomeContext>();
+    var migrator = ctx.GetInfrastructure().GetService<IMigrator>();
+
+    migrator.Migrate();
     ctx.Database.EnsureDeleted();
     ctx.Database.EnsureCreated();
+    ctx.Persons.Add(new Person { PrimaryAddress = new Address(), SecondaryAddress = new Address()});
+    ctx.Persons.Add(new Person { PrimaryAddress = new Address()});
+    ctx.Addresses.Add(new Address());
     ctx.Images.Add(new Image
     {
-        Path = "qwerty",
+        Path = "qwerty"
     });
     ctx.Messages.Add(new Message
     {
         MessageText = "qwerty",
         Images = new List<Image>
         {
-            new Image
+            new()
             {
-                Path = "qwerty",
-            },
+                Path = "qwerty"
+            }
         }
     });
     ctx.SaveChanges();
@@ -38,6 +48,9 @@ using (var scope = serviceProvider.CreateScope())
 {
     var ctx = scope.ServiceProvider.GetService<SomeContext>();
     var message = ctx.Messages.First();
+    var addresses = ctx.Addresses
+        .Include(a => a.SecondaryPeopleAddresses)
+        .ToList();
     var listAsync = await ctx.Images
         .Where(i => !i.Messages.Any() || i.Messages.Any(m => m.MessageId == message.MessageId))
         .ToListAsync();
@@ -51,6 +64,20 @@ public class SomeContext : DbContext
 
     public DbSet<Message> Messages { get; set; }
     public DbSet<Image> Images { get; set; }
+
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>()
+            .HasOne(person => person.PrimaryAddress)
+            .WithMany(a => a.PrimaryPeopleAddresses);
+
+        modelBuilder.Entity<Person>()
+            .HasOne(person => person.SecondaryAddress)
+            .WithMany(a => a.SecondaryPeopleAddresses);
+    }
 }
 
 public class Message
@@ -62,10 +89,28 @@ public class Message
 
 public class Image
 {
-    public int ImageId {get;set;}
+    public int ImageId { get; set; }
 
-    [Required, StringLength(2048)]
-    public string Path {get;set;}
+    [Required] [StringLength(2048)] public string Path { get; set; }
 
     public virtual ICollection<Message> Messages { get; set; }
 }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required] 
+        public int? PrimaryAddressId { get; set; }
+        public Address? PrimaryAddress { get; set; }
+
+        public int? SecondaryAddressId { get; set; }
+        public Address? SecondaryAddress { get; set; }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public List<Person> PrimaryPeopleAddresses { get; set; } = new();
+        public List<Person> SecondaryPeopleAddresses { get; set; } = new();
+    }
--- csharp/SOAnswers/SOAnswers/Program.cs
+++ csharp/SOAnswers/SOAnswers/Program.cs
@@ -1,3 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+using System.Reactive.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Moq;
+
+Console.WriteLine(UInt32.MaxValue);
+
+Console.WriteLine(Convert.ToUInt32("111",2));
+Console.WriteLine();
+public class BaseValidator
+{
+    public BaseValidator(Context context) {}
+}
+
+public class Context:IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
+public interface IContextProvider 
+{
+    Context Create();
+}
+
+
+
+public class EntityValidator: BaseValidator , IDisposable
+{
+    private readonly Context _context;
+    public EntityValidator(IContextProvider provider) : this(new CaptureContextProvider(provider)) 
+    {
+    }
+    
+    private EntityValidator(CaptureContextProvider provider) : base(provider.Create()) 
+    {
+        _context = provider.Create();
+    }
+    class CaptureContextProvider : IContextProvider
+    {
+        private readonly IContextProvider _contextProvider;
+        private Context? _capture;
+
+        public CaptureContextProvider(IContextProvider contextProvider)
+        {
+            _contextProvider = contextProvider;
+        }
+
+        public Context Create()
+        {
+            _capture ??= _contextProvider.Create();
+            return _capture;
+        }
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
