aa5846c7a2ead44b3ef66c53defd3ac283697fa1
gurustron <gurustronpublic@gmail.com>
2023-05-05 21:44:26 +0300

    Leetcode SpiralMatrix2.cs

---
+++ csharp/LeetCode/LeetCode/Study/DataStructure/DataStructureTwo/SpiralMatrix2.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace LeetCode.Study.DataStructure.DataStructureTwo;
+
+public class SpiralMatrix2
+{
+    public int[][] GenerateMatrix(int n)
+    {
+        var result = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = new int[n];
+        }
+
+        int counter = 1;
+        for (int diagI = 0, j = n-1; diagI <= j; diagI++, j--)
+        {
+            var diff = j - diagI;
+            for (int i = 0; i <= j; i++)
+            {
+                var @base = counter + i;
+                result[diagI][diagI + i] = @base;
+                result[diagI + i][j] = @base + diff;
+                result[j][j - i] = @base + 2 * diff;
+                result[j - i][diagI] = @base + 3 * diff;
+            }
+
+            counter += diff * 4 + 1;
+        }
+
+        return result;
+    }
+}
