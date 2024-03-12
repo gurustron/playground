7c01ae6b35c0ede32a123ec8b7f84842fdae58cb
gurustron <gurustronpublic@gmail.com>
2024-03-12 21:26:08 +0300

    interceptor demo

---
+++ csharp/SOAnswers/NET8/WhatsNew/WhatsNewCSharp12.Interceptors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+var c = new C();
+c.InterceptableMethod(1); // (L1,C1): prints "interceptor 1"
+c.InterceptableMethod(1); // (L2,C2): prints "other interceptor 1"
+c.InterceptableMethod(2); // (L3,C3): prints "other interceptor 2"
+c.InterceptableMethod(1); // prints "interceptable 1"
+
+class C
+{
+    public void InterceptableMethod(int param)
+    {
+        Console.WriteLine($"interceptable {param}");
+    }
+}
+
+// generated code
