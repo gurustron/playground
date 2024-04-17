36d23b495b39263bc9e6ad9fa65279057636fb22
gurustron <gurustronpublic@gmail.com>
2024-04-17 22:04:00 +0300

    kafka + test containers

---
+++ csharp/KafkaPlayground/KafkaPlayground/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "127.0.0.1:9092",
+
+};
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    var result = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value="a log message" });
+}
+// using (var producer = new ProducerBuilder<string, string>(config).Build())
+// {
+//     await producer.ProduceAsync("test-topic", new Message<string, string>
+//     {
+//         Key = "someNewKey",
+//         Value = """
+//                 {
+//                     "Hahah":"nice"
+//                 }
+//                 """
+//     });
+// }
