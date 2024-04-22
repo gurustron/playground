e5ca85412c2bb6d3412db859433a2f08c504bef2
gurustron <gurustronpublic@gmail.com>
2024-04-22 22:51:23 +0300

    GRPC started

---
+++ csharp/GrpcPlayground/GrpcPlayground.TestClient/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcPlayground;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("http://localhost:5054");
+
+var greeterClient = new Greeter.GreeterClient(channel);
+
+var helloReply =await greeterClient.SayHelloAsync(new HelloRequest(){Name = "qweqeqw"});
+Console.WriteLine(helloReply.Message);
