6575fb5c1bea3b85b07c100e4fe471e1dea1079b
gurustron <gurustronpublic@gmail.com>
2024-05-16 22:10:57 +0300

    kafka + a bit of progress

--- csharp/KafkaPlayground/KafkaPlayground/Program.cs
+++ csharp/KafkaPlayground/KafkaPlayground/Program.cs
@@ -1,26 +1,31 @@
 ﻿using System.Text;
 using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
 
 var config = new ProducerConfig
 {
     BootstrapServers = "127.0.0.1:9092",
+
 };
-// using (var producer = new ProducerBuilder<Null, string>(config).Build())
-// {
-//     var result = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value="a log message" });
-// }
-// using (var producer = new ProducerBuilder<string, string>(config).Build())
-// {
-//     await producer.ProduceAsync("test-topic", new Message<string, string>
-//     {
-//         Key = "someNewKey",
-//         Value = """
-//                 {
-//                     "Hahah":"nice"
-//                 }
-//                 """
-//     });
-// }
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    var result = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value="a log message" });
+}
+
+var producerBuilder = new ProducerBuilder<string, string>(config);
+// producerBuilder.SetKeySerializer(Serializers.Utf8);
+using (var producer = producerBuilder.Build())
+{
+    await producer.ProduceAsync("test-topic", new Message<string, string>
+    {
+        Key = "someNewKey",
+        Value = """
+                {
+                    "Hahah":"nice"
+                }
+                """
+    });
+}
 var consumerConfig = new ConsumerConfig
 {
     BootstrapServers = "127.0.0.1:9092",
--- csharp/KafkaPlayground/Readme.md
+++ csharp/KafkaPlayground/Readme.md
@@ -22,5 +22,17 @@ docker exec -i kafkakraft kafka-console-consumer --topic kinaction_helloworld --
 ```
 remove `--from-beginning` to get only new ones since start
 
+##### Kafka APIs 
+- KafkaStreams
+  - This API was made to ensure that creating streaming applications is as easy as possible,
+  - Kafka Streams takes the core parts of Kafka and works on top of those smaller pieces by adding stateful processing and distributed joins, for example, without much more complexity or overhead 
+- Kafka Connect
+  - Is designed to integrate well with stream-processing frameworks to copy data. 
+  - Is an excellent choice for making quick and simple data pipelines that tie together common systems.
+- AdminClient package
+- ksqlDB
+  - SQL engine for Kafka
+  - Queries are continuously running and updating
+- 
 
-Stopped at 2.4 Various source code packages and what they do p.55
+Stopped at 2.6 Stream processing and terminology p.61
